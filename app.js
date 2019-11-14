8ced908b5942eb5ff58908d559dc80e3af136442
dpatel0114 <1991dharapatel@gmail.com>
2019-11-14 12:57:50 -0500

    working calculator

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 document.getElementById('loan-form').addEventListener('submit', calculateResults)
 
 function calculateResults(e){
+    e.preventDefault()
+
     // UI Vars 
     const amount = document.getElementById('amount')
     const interest = document.getElementById('interest')
@@ -11,24 +13,48 @@ function calculateResults(e){
     const totalInterest = document.getElementById('total-interest')
 
     const principal = parseFloat(amount.value)
-    const calculatedInterest = parseFloat(interest.value) /100 /12
+    const calculatedInterest = parseFloat(interest.value) / 100 / 12
     const calculatedPayments = parseFloat(years.value) * 12
 
 
     // Coumpute monthly payment 
     const x = Math.pow(1 + calculatedInterest, calculatedPayments)
-    const monthly = (principal*X*calculatedInterest) / (x-1)
+    const monthly = (principal*x*calculatedInterest) / (x-1)
 
     if(isFinite(monthly)){
-
-    }else {
         monthlyPayment.value = monthly.toFixed(2)
         totalPayment.value = (monthly * calculatedPayments).toFixed(2)
         totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2)
+    }else {
+        showError('Please chek your numbers')
+    //   console.log('please check your numbers')
     }
 
+    // Show Error 
+    function showError(error){
+        // create div 
+        const errorDiv = document.createElement('div')
 
+        // get element
+        const card = document.querySelector('.card')
+        const heading = document.querySelector('.heading')
 
-    e.preventDefault()
+        // ad class 
+        errorDiv.className = 'alert alert-danger'
+
+        // create text node and append to div 
+        errorDiv.appendChild(document.createTextNode(error))
+
+        //insert error above heading 
+        card.insertBefore(errorDiv, heading)
+
+        // clear error after 3 sec
+        setTimeout(clearError, 3000)
+
+        function clearError(){
+            document.querySelector('.alert').remove()
+        }
+    }
+
 
 }
